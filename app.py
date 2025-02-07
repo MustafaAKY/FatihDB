661bc6db29f3d36f76946e3afea956ed31c46169
MustafaAKY <144323635+MustafaAKY@users.noreply.github.com>
2025-02-08 00:06:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -508,7 +508,7 @@ with tab33:
 
                 liste.append({"TAKİP KODU":veriler[4], 
                   "İSİM SOYİSİM":veriler[5],
-                  "TELEFON NU":veriler[17],
+                  "TELEFON NU":veriler[19],
                   "SONUÇ":veriler[9],
                   "KARGO ŞUBESİ":veriler[10],
                   "ÜCRET":veriler[13]+" TL"  })   
